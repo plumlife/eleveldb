3204abc18d7e8ff0240c067b48a3cf40422b7b65
Parnell Springmeyer <parnell@digitalmentat.com>
2015-05-15 18:00:13 -0500

    Okay, let's see if this does the trick

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -64,6 +64,9 @@ case "$1" in
             tar -xzf snappy-$SNAPPY_VSN.tar.gz
             (cd snappy-$SNAPPY_VSN && ./configure --prefix=$BASEDIR/system --libdir=$BASEDIR/system/lib --with-pic)
         fi
+        
+        export CC="/usr/bin/arm-linux-gnueabi-gcc"
+        export CXX="/usr/bin/arm-linux-gnueabi-cpp"
 
         (cd snappy-$SNAPPY_VSN && $MAKE && $MAKE install)
 
