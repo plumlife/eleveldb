b5ee979523e4d8cd6b55a6aa7432ba9ea12f7355
Parnell Springmeyer <parnell@digitalmentat.com>
2015-05-26 10:53:52 -0500

    Improving the shell script and overriding more compiler toolchain variables

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export PATH=/root/x-tools/arm-plum-linux-gnueabi/bin:$PATH
+export PATH="/root/x-tools/arm-plum-linux-gnueabi/bin:${PATH}"
+
+CROSS="arm-plum-linux-gnueabi"
 
 LEVELDB_VSN="1.18"
 SNAPPY_VSN="1.0.4"
@@ -12,7 +14,7 @@ if [ `basename $PWD` != "c_src" ]; then
     pushd c_src
 fi
 
-BASEDIR="$PWD"
+BASEDIR="${PWD}"
 
 case "$1" in
     clean)
@@ -20,35 +22,39 @@ case "$1" in
         ;;
 
     test)
-        export CFLAGS="$CFLAGS -I $BASEDIR/system/include"
-        export LDFLAGS="$LDFLAGS -L $BASEDIR/system/lib"
-        export LD_LIBRARY_PATH="$BASEDIR/system/lib:$LD_LIBRARY_PATH"
+        export CFLAGS="${CFLAGS} -I ${BASEDIR}/system/include"
+        export LDFLAGS="${LDFLAGS} -L ${BASEDIR}/system/lib"
+        export LD_LIBRARY_PATH="${BASEDIR}/system/lib:${LD_LIBRARY_PATH}"
 
         (cd leveldb && make check)
 
         ;;
     *)
-        if [ ! -d snappy-$SNAPPY_VSN ]; then
-            tar -xzf snappy-$SNAPPY_VSN.tar.gz
-            (cd snappy-$SNAPPY_VSN && ./configure --host=arm-linux-gnueabi --prefix=$BASEDIR/system --libdir=$BASEDIR/system/lib --with-pic)
+        if [ ! -d "snappy-${SNAPPY_VSN}" ]; then
+            tar -xzf "snappy-${SNAPPY_VSN}.tar.gz"
+            (cd "snappy-${SNAPPY_VSN}" && ./configure --host="${CROSS}" --prefix="${BASEDIR}/system" --libdir="${BASEDIR}/system/lib" --with-pic)
         fi
         
-        export CC="arm-plum-linux-gnueabi-gcc"
-        export CXX="arm-plum-linux-gnueabi-g++"
-        export AR="arm-plum-linux-gnueabi-ar"
-        export RANLIB="arm-plum-linux-gnueabi-ranlib"
+        export CC="${CROSS}-gcc"
+        export CXX="${CROSS}-g++"
+        export AR="${CROSS}-ar"
+        export RANLIB="${CROSS}-ranlib"
+        export LD="${CROSS}-ld"
+        export LDD="${CROSS}-ldd"
+        export ELFEDIT="${CROSS}-elfedit"
+        export STRIP="${CROSS}-strip"
 
-        (cd snappy-$SNAPPY_VSN && $MAKE && $MAKE install)
+        (cd "snappy-${SNAPPY_VSN}" && $MAKE && $MAKE install)
 
-        export CFLAGS="$CFLAGS -I $BASEDIR/system/include"
-        export CXXFLAGS="$CXXFLAGS -I $BASEDIR/system/include"
-        export LDFLAGS="$LDFLAGS -L$BASEDIR/system/lib"
-        export LD_LIBRARY_PATH="$BASEDIR/system/lib:$LD_LIBRARY_PATH"
+        export CFLAGS="${CFLAGS} -I ${BASEDIR}/system/include"
+        export CXXFLAGS="${CXXFLAGS} -I ${BASEDIR}/system/include"
+        export LDFLAGS="${LDFLAGS} -L${BASEDIR}/system/lib"
+        export LD_LIBRARY_PATH="${BASEDIR}/system/lib:${LD_LIBRARY_PATH}"
         export TARGET_OS="OS_LINUX_ARM_CROSSCOMPILE"
 
         if [ ! -d leveldb ]; then
             git clone git://github.com/plumlife/leveldb
-            (cd leveldb && git checkout ARM32-$LEVELDB_VSN)
+            (cd leveldb && git checkout "ARM32-${LEVELDB_VSN}")
         fi
 
         (cd leveldb && $MAKE all)
